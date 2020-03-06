6d53cc6403581ab9f18edb10cbcf7e846f593fa5
Shyngys <Shyngys>
2020-03-06 21:52:09 +0600

    Initial Commit

---
+++ lab3_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab3_3
+//
+//  Created by Шынгыс on 3/6/20.
+//  Copyright © 2020 Шынгыс. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
